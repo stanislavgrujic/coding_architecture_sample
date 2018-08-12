0604acb5ee971c1bd435a4211f466b690e84e815
stanislavgrujic <sgrujic@execom.eu>
2018-08-12 16:31:16 +0200

    Fix character casing

--- src/main/java/com/stanislavgrujic/codingarchitecture/CodingArchitectureApplication.java
+++ src/main/java/com/stanislavgrujic/codingarchitecture/CodingArchitectureApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodingarchitectureApplication {
+public class CodingArchitectureApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CodingarchitectureApplication.class, args);
+		SpringApplication.run(CodingArchitectureApplication.class, args);
 	}
 }
--- src/test/java/com/stanislavgrujic/codingarchitecture/CodingArchitectureApplicationTests.java
+++ src/test/java/com/stanislavgrujic/codingarchitecture/CodingArchitectureApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CodingarchitectureApplicationTests {
+public class CodingArchitectureApplicationTests {
 
 	@Test
 	public void contextLoads() {
